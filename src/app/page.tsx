b4ed26d9f0463f035ebc8b8b281a350795a195e2
Rohan Coder <116200861+rohanhumai@users.noreply.github.com>
2025-07-13 13:28:55 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 import React from 'react'
 import Dashboard from '@/app/dashboard/page'
+import Creations from '@/app/Creations/page'
+import Pricing from "@/app/pricing/page"
 
 const page = () => {
   return (
     <>
     <Dashboard/>
+    <Creations/>
+    <Pricing/>
     </>
   )
 }
